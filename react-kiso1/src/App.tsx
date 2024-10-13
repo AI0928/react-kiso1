60fcc3e451006bd377b5c6fd6d941af705832cdb
AI0928 <toya.kannnatuki@gmail.com>
2024-10-14 06:10:54 +0900

    フェッチしたデータを表示できるようにした

--- react-kiso1/src/App.tsx
+++ react-kiso1/src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+type PostData = {
+  id: string,
+  title: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<PostData[]>([])
+
+  //データ取得
+  async function getData(n: string) {
+    const url = "https://railway.bulletinboard.techtrain.dev/threads?offset=" + n;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`レスポンスステータス: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      setData(json)
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    const n = "10";
+    getData(n);
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>データ一覧</h1>
+        <div className="card-container">       
+          {data.map((item, index) => (
+            <div className="card" key={index}>
+              <h2 className="card-title">{item.title}</h2>
+            </div>
+          ))}
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
